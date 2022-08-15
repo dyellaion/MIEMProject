05523a23407be1526c137c33e7dd079ae51dc179
dyellaion <lapsm@mail.ru>
2022-08-15 23:18:33 +0300

    migration added

--- Backend/BackendAPI/Migrations/20220815201736_SeedingDB.Designer.cs
+++ Backend/BackendAPI/Migrations/20220815201736_SeedingDB.Designer.cs
@@ -2,15 +2,17 @@
 using BackendAPI.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BackendAPI.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20220815201736_SeedingDB")]
+    partial class SeedingDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
