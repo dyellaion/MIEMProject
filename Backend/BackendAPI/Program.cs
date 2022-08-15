a133b838291ad52e43f0c3cc2e7fa1629c56a07a
dyellaion <lapsm@mail.ru>
2022-08-15 16:36:02 +0300

    add logging

--- Backend/BackendAPI/Program.cs
+++ Backend/BackendAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BackendAPI
 {
@@ -9,10 +10,17 @@ namespace BackendAPI
         {
             CreateHostBuilder(args).Build().Run();
         }
-        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
