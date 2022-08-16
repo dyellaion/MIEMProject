e91e63da42753ef7c95a66ebc74d62a0dcbb3e36
M3RCURRRY <49792824+M3RCURRRY@users.noreply.github.com>
2022-08-16 04:38:55 +0300

    Creation form UX

--- Frontend/panel/src/pages/CreationPage/CreationPage.tsx
+++ Frontend/panel/src/pages/CreationPage/CreationPage.tsx
@@ -1,4 +1,16 @@
-import { Badge, Chip, Container, Button } from "@mui/material";
+import {
+  Badge,
+  Chip,
+  Container,
+  Button,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 import React, { ChangeEvent, FormEvent } from "react";
 import { Script } from "../../types/response";
 
@@ -12,20 +24,24 @@ class CreationPage extends React.Component<{}, Script> {
     lang: "",
   };
 
-  submitHandler(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  submitHandler() {
     if (this.state.scriptName === "" || this.state.path === "") {
       alert("Something is empty");
     }
   }
 
-  changeHandler(e: ChangeEvent<HTMLInputElement>) {
+  changeHandler(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
     switch (e.target.name) {
       case "name":
-        console.log(e.target.value);
+        console.log(this.state.scriptName);
+        this.setState({
+          scriptName: e.target.value,
+        });
         break;
       case "path":
-        console.log(e.target.files?.[0] ?? "No files selected!");
+        this.setState({
+          path: e.target.value,
+        });
         break;
       case "lang":
         console.log(e.target.value);
@@ -33,19 +49,65 @@ class CreationPage extends React.Component<{}, Script> {
     }
   }
 
+  selectHandler(e: SelectChangeEvent<string>) {
+    console.log(e.target.value);
+    this.setState({
+      lang: e.target.value
+    })
+  }
+
   render(): React.ReactNode {
     return (
-      <Container>
-        <form onSubmit={(e) => this.submitHandler(e)} className={styles.formLayout}>
+      <Container
+        sx={{ textAlign: "center", mt: "25px" }}
+        className={styles.containerLayout}
+      >
+        <Typography variant="h5" component="div" className={styles.typography}>
+          Add new script
+        </Typography>
+        <form
+          className={styles.formLayout}
+        >
           <label className={styles.labelLayout}>
-            <Chip label="Script name" variant="filled" color="info" />
-            <input type="text" name="name" value="" placeholder="Script maiden name" onChange={(e) => this.changeHandler(e)}/>
+            <Chip label="Script name" color="info" />
+            <TextField
+              name="name"
+              id="standard-basic"
+              label="Script name"
+              variant="standard"
+              value={this.state.scriptName}
+              onChange={(e) => this.changeHandler(e)}
+            />
           </label>
           <label className={styles.labelLayout}>
-            <Chip label="Script path" variant="filled" color="info" />
-            <input type="file" name="path" onChange={(e) => this.changeHandler(e)}/>
+            <Chip label="Script path" color="info" />
+            <TextField
+              name="path"
+              id="standard-basic"
+              label="Script path"
+              variant="standard"
+              value={this.state.path}
+              onChange={(e) => this.changeHandler(e)}
+            />
           </label>
-          <Button variant="contained" sx={{ m: "10px" }}>
+          <label className={styles.labelLayout}>
+            <Chip label="File extension" color="info" />
+            <FormControl fullWidth sx={{mt: "20px"}}>
+              <InputLabel id="lang-label">Ext</InputLabel>
+              <Select
+                labelId="lang-label"
+                name="lang"
+                value={this.state.lang}
+                label="Ext"
+                onChange={(e) => this.selectHandler(e)}
+              >
+                <MenuItem value={"py"}>Python</MenuItem>
+                <MenuItem value={"rb"}>Ruby</MenuItem>
+                <MenuItem value={"c"}>C</MenuItem>
+              </Select>
+            </FormControl>
+          </label>
+          <Button variant="contained" sx={{ m: "10px" }} onClick={() => this.submitHandler()}>
             Save
           </Button>
         </form>
