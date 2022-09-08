287bc707c5911d284763be60575c41006cab9661
dyellaion <lapsm@mail.ru>
2022-09-08 12:16:07 +0300

    new migration

---
+++ Backend/BackendAPI/Migrations/20220908090153_UpdateRunning.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendAPI.Migrations
+{
+    public partial class UpdateRunning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ScriptName",
+                table: "ScriptsRunning",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScriptName",
+                table: "ScriptsRunning");
+        }
+    }
+}
